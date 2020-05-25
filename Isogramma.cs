6e8f2d90f8d6e81969b7c56827255e464683137e
Filo-Cesa <filicesarini@gmail.com>
2020-05-25 17:16:12 +0200

    .

---
+++ Isogramma.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogramma
+{
+    public static bool Verifica(string word)
+    {
+        // Convert the string in lower case letters 
+        word = word.ToLower(); 
+       word = word.Replace("-", "");
+       word = word.Replace(" ", "");
+
+        int len = word.Length; 
+  
+        char[] arr = word.ToCharArray(); 
+  
+        Array.Sort(arr); 
+        for (int i = 0; i < len - 1; i++) { 
+            if (arr[i] == arr[i + 1]) 
+                return false; 
+        } 
+        return true;
+    } 
+}
